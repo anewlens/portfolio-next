262a422d8847997e8f0f08f7993ca8961d654be7
anewlens <36173414+anewlens@users.noreply.github.com>
2022-03-29 22:43:21 -0400

    Page Transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../Components/Layout/Layout'
+import {
+  AnimatePresence,
+  domAnimation, LazyMotion,
+  m
+} from 'framer-motion'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+
+  const pageAnimation = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 }
+  }
+
+
   return (
     <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence initial={false} exitBeforeEnter>
+          <m.main
+            key={router.route}
+            variants={pageAnimation}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ type: 'linear' }}>
+            <Component {...pageProps} />
+          </m.main>
+        </AnimatePresence>
+
+      </LazyMotion>
+    </Layout >
   )
 }
 
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { NextPage } from 'next'
+import styles from '../../styles/Pages/Work.module.css'
+import Button from '../../Components/Common/NextPageLink';
 
 const Work: NextPage = () => {
     return (
-        <div>
-            <h1>Work</h1>
+        <div className={styles.content}>
+            <h1 className={styles.content_title}>Work</h1>
+            <Button route="/contact">
+                Wanna talk?
+            </Button>
         </div>
     );
 };
