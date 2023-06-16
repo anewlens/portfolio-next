087d05ec5f3201122fbb394bf67b9c008e3fc7c8
Robbie Carroll <rccarroll3@gmail.com>
2023-06-16 18:21:14 -0400

    Update history

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
-import Head from 'next/head';
-import { NextPage, } from 'next'
-import styles from '../../styles/Pages/About.module.css'
+import React from "react";
+import Head from "next/head";
+import { NextPage } from "next";
+import styles from "../../styles/Pages/About.module.css";
 
 const About: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>About | Robbie Carroll</title>
-                <meta name="description" content="Robbie Carroll, web dev and designer based in Richmond, VA + remote" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
-                <link rel="manifest" href="/img/icons/site.webmanifest" />
-            </Head>
-            <div className={styles.content}>
-                <h1 className={styles.content_title}>About</h1>
-                <p className={styles.content_para}>Hi, I’m Robbie, a web developer and sometimes designer in  Richmond, VA. Currently I’m building UI and department sites at <a href="/img/iconshttps://medschool.vcu.edu" rel='noreferrer' target="_blank" > VCU School of Medicine</a> and working on BiteBook, a recipe repository app.</p>
-                <p className={styles.content_para}>Out of college, I started as a copywriter,  but have worked as a self-taught developer since 2019, working within the TerminalFour CMS by day and React by night. </p>
-                <p className={styles.content_para}>On the weekend, I do light woodworking, build speakers and play D&D. </p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>About | Robbie Carroll</title>
+        <meta
+          name="description"
+          content="Robbie Carroll, web dev and designer based in Richmond, VA + remote"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/img/icons/site.webmanifest" />
+      </Head>
+      <div className={styles.content}>
+        <h1 className={styles.content_title}>About</h1>
+        <p className={styles.content_para}>
+          Hi, I’m Robbie, a software engineer and sometimes designer in
+          Richmond, VA, currently building fullstack applications at Capital One
+          and in my free time developing BiteBook, a recipe repository app.
+        </p>
+        <p className={styles.content_para}>
+          Out of college, I started as a copywriter, but have worked as a
+          self-taught developer since 2019, working with Typescript, React,
+          NodeJS and AWS.{" "}
+        </p>
+        <p className={styles.content_para}>
+          On the weekend, I do light woodworking, build speakers, tinker with
+          synths and play D&D.{" "}
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -1,71 +1,95 @@
-import React from 'react';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../../styles/Pages/Skills.module.css'
+import React from "react";
+import { NextPage } from "next";
+import Head from "next/head";
+import styles from "../../styles/Pages/Skills.module.css";
 
 const Skills: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title>Skills | Robbie Carroll</title>
-                <meta name="description" content="Robbie Carroll, web dev and designer based in Richmond, VA + remote" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
-                <link rel="manifest" href="/img/icons/site.webmanifest" />
-            </Head>
-            <div className={styles.content}>
-                <h1 className={styles.content_title}>Skills</h1>
-                <section className={styles.content_section}>
-                    <h2 className={styles.content_section_title}>Development</h2>
-                    <ul className={styles.content_list}>
-                        <li>HTML</li>
-                        <li>CSS <ul>
-                            <li>Sass</li>
-                            <li>CSS Modules</li>
-                        </ul></li>
-                        <li>Javascript
-                            <ul>
-                                <li>Typescript</li>
-                            </ul>
-                        </li>
-                        <li>React
-                            <ul>
-                                <li>Redux</li>
-                                <li>Context</li>
-                                <li>NextJS</li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul className={styles.content_list}>
-                        <li>Node
-                            <ul>
-                                <li>Express</li>
-                            </ul>
-                        </li>
-                        <li>MongoDB</li>
-                        <li>SQL</li>
-                    </ul>
-                </section>
+  return (
+    <>
+      <Head>
+        <title>Skills | Robbie Carroll</title>
+        <meta
+          name="description"
+          content="Robbie Carroll, web dev and designer based in Richmond, VA + remote"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/img/icons/site.webmanifest" />
+      </Head>
+      <div className={styles.content}>
+        <h1 className={styles.content_title}>Skills</h1>
+        <section className={styles.content_section}>
+          <h2 className={styles.content_section_title}>Development</h2>
+          <ul className={styles.content_list}>
+            <li>HTML / CSS</li>
+            <li>
+              Javascript
+              <ul>
+                <li>Typescript</li>
+              </ul>
+            </li>
+            <li>
+              React
+              <ul>
+                <li>NextJS</li>
+              </ul>
+            </li>
+            <li>
+              Testing{" "}
+              <ul>
+                {" "}
+                <li>Cypress</li>
+                <li>Jest</li>
+              </ul>
+            </li>
+          </ul>
+          <ul className={styles.content_list}>
+            <li>
+              Node
+              <ul>
+                <li>Express</li>
+              </ul>
+            </li>
+            <li>AWS</li>
+            <li>MongoDB</li>
+            <li>SQL</li>
+          </ul>
+        </section>
 
-                <section className={styles.content_section}>
-                    <h2 className={styles.content_section_title}>Design</h2>
-                    <ul className={styles.content_list}>
-                        <li>Figma</li>
-                        <li>Adobe <ul>
-                            <li>Photoshop</li>
-                            <li>InDesign</li>
-                            <li>Illustrator</li>
-                            <li>Premier</li>
-                            <li>XD</li>
-                        </ul></li>
-
-                    </ul>
-
-                </section>
-            </div>
-        </>
-    );
+        <section className={styles.content_section}>
+          <h2 className={styles.content_section_title}>Design</h2>
+          <ul className={styles.content_list}>
+            <li>Figma</li>
+            <li>
+              Adobe{" "}
+              <ul>
+                <li>Photoshop</li>
+                <li>InDesign</li>
+                <li>Illustrator</li>
+                <li>Premier</li>
+                <li>XD</li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
